775748cf0ec08c1f8f8e70f4a55b418b2086bf7b
Ryan <ryanrchesla@gmail.com>
2013-10-28 12:06:03 -0400

    add monitor device

--- src/DeviceManager.java
+++ src/DeviceManager.java
@@ -1,7 +1,12 @@
 import java.util.HashMap;
+import java.util.Random;
 
 
 public class DeviceManager {
+	
+	public static final int DEVICE_STATUS_OK = 1;
+	public static final int DEVICE_STATUS_UNRESPONSIVE = 2;
+	
 	private HashMap<String,Device>mapOfDevices;
 
 	public DeviceManager() {
@@ -14,10 +19,23 @@ public class DeviceManager {
 		System.out.println("Device Registered with ID:" + device.getDeviceID() + ", ip address: " + device.getIpAddress()+", port number:" + device.getPortNumber());
 	}
 	
-	
-	
-	public void checkPingDevice(String deviceId){
+	public void monitorDevice(String deviceId){
 		Device device = mapOfDevices.get(deviceId);
+		
+		Random r = new Random();
+		int whenToMonitor=r.nextInt(60000-20000) + 20000;
+		
+		System.out.println("Device: "+ device.getDeviceID() + "will be monitored every " + whenToMonitor/1000 + " seconds");
+		MonitorWorker monitorWorker = new MonitorWorker(device, this, whenToMonitor); //monitor every 60 seconds
+	}
+	
+	public void reportDeviceStatus(Device device, int deviceStatus){
+		if (deviceStatus == DeviceManager.DEVICE_STATUS_OK){
+			System.out.println("Device: "+ device.getDeviceID() + " is OK");
+		}
+		else if (deviceStatus == DeviceManager.DEVICE_STATUS_UNRESPONSIVE){
+			System.out.println("Device: "+ device.getDeviceID() + " is unresponsive");
+		}
 	}
 	
 	
---
+++ src/MonitorWorker.java
@@ -0,0 +1,40 @@
+
+public class MonitorWorker extends Thread{
+	
+	Device deviceToMonitor;
+	DeviceManager deviceManager;
+	int howOftenToMonitorInMilliSeconds;
+	
+	public MonitorWorker(Device deviceToMonitor, DeviceManager deviceManger, int howOftenToMonitorInMilliSeconds){
+	
+		this.deviceToMonitor = deviceToMonitor;
+		this.deviceManager = deviceManger;
+		this.howOftenToMonitorInMilliSeconds = howOftenToMonitorInMilliSeconds;
+		this.start();
+	}
+	
+
+	
+	@Override
+	public void run() {
+		//need to check device's status
+		while (true){
+			
+			//if ok
+			this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_OK);
+			
+			//if unresponsive
+			this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
+			
+			try {
+				//sleep until its time to monitor again
+				Thread.sleep(howOftenToMonitorInMilliSeconds);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
+}
