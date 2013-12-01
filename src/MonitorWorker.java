d0ef4ad827b421975ed3a01acce0a810b67ce058
john <john@johngaughan.net>
2013-12-01 16:58:17 -0500

    Formatted source for readability. Converted thread usage to Java 5 executor framework.

--- src/MonitorWorker.java
+++ src/MonitorWorker.java
@@ -3,110 +3,100 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Timer;
 import java.util.TimerTask;
 
+public class MonitorWorker implements Runnable {
 
-public class MonitorWorker extends Thread{
-	
-	Device deviceToMonitor;
-	DeviceManager deviceManager;
-	int howOftenToMonitorInMilliSeconds;
-	
-	public MonitorWorker(Device deviceToMonitor, DeviceManager deviceManger, int howOftenToMonitorInMilliSeconds){
-		this.deviceToMonitor = deviceToMonitor;
-		this.deviceManager = deviceManger;
-		this.howOftenToMonitorInMilliSeconds = howOftenToMonitorInMilliSeconds;
-	}
-	
+  Device deviceToMonitor;
+  DeviceManager deviceManager;
+  int howOftenToMonitorInMilliSeconds;
 
-	
-	@Override
-	public void run() {
-		//need to check device's status
-		while (true){
-			
-			
-			try {
-				//sleep until its time to monitor again
-				Thread.sleep(howOftenToMonitorInMilliSeconds);
-			} catch (InterruptedException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			
-			//connect to client
-			Socket socketServer = null;
-		
-			
-			try{
-				final TimerExtension timer = new TimerExtension();
-				timer.hasConnected =false;
-				timer.schedule(new TimerTask() {
-					  @Override
-					  public void run() {
-					    //if device doenst connect in alloted time its unresponsive
-						  if (timer.hasConnected == false){
-							  deviceManager.reportDeviceStatus(deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
-						  }
-						  
-					  }
-					}, howOftenToMonitorInMilliSeconds); 
-				
-				
-				socketServer = new Socket(this.deviceToMonitor.getIpAddress(),this.deviceToMonitor.getPortNumber());
-				timer.hasConnected = true;
+  public MonitorWorker(Device deviceToMonitor, DeviceManager deviceManger, int howOftenToMonitorInMilliSeconds) {
+    this.deviceToMonitor = deviceToMonitor;
+    this.deviceManager = deviceManger;
+    this.howOftenToMonitorInMilliSeconds = howOftenToMonitorInMilliSeconds;
+  }
 
-			} catch (UnknownHostException e) {
-				   System.out.println("Unknown host " + e.getMessage());
-					//if unresponsive
-					this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
-			   } catch  (IOException e) {
-				   System.out.println("IO Exception" + e.getMessage());
-					//if unresponsive
-					this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
-			   }
-			
-			String isDeviceOkay = "";
-			BufferedReader in = null;
-		
-			try {
-				in = new BufferedReader(new InputStreamReader(socketServer.getInputStream()));
+  @Override
+  public void run() {
+    // need to check device's status
+    while (true) {
 
-				isDeviceOkay = in.readLine();
-				int deviceOkayInt = Integer.parseInt(isDeviceOkay);
-				
-				if (deviceOkayInt == 1){
+      try {
+        // sleep until its time to monitor again
+        Thread.sleep(howOftenToMonitorInMilliSeconds);
+      }
+      catch (InterruptedException e) {
+        System.out.println(e.getMessage());
+      }
 
-					//if ok
-					this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_OK);
-				}
-				else{
-					//if not ok
-					this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_NOT_OKAY);
-				}
-				
-			}
-			catch (Exception e){
-				System.out.println(e.getMessage());
-				//if not ok
-				this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_NOT_OKAY);
-				
-			}
-			
-			
-			try {
-				socketServer.close();
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			
-			
-			
-		}
-		
-		
-		
-	}
+      // connect to client
+      Socket socketServer = null;
+
+      try {
+        final TimerExtension timer = new TimerExtension();
+        timer.hasConnected = false;
+        timer.schedule(new TimerTask() {
+          @Override
+          public void run() {
+            // if device doesn't connect in alloted time it's unresponsive
+            if (timer.hasConnected == false) {
+              deviceManager.reportDeviceStatus(deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
+            }
+
+          }
+        }, howOftenToMonitorInMilliSeconds);
+
+        socketServer = new Socket(this.deviceToMonitor.getIpAddress(), this.deviceToMonitor.getPortNumber());
+        timer.hasConnected = true;
+
+      }
+      catch (UnknownHostException e) {
+        System.out.println("Unknown host " + e.getMessage());
+        // if unresponsive
+        this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
+      }
+      catch (IOException e) {
+        System.out.println("IO Exception" + e.getMessage());
+        // if unresponsive
+        this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_UNRESPONSIVE);
+      }
+
+      String isDeviceOkay = "";
+      BufferedReader in = null;
+
+      try {
+        in = new BufferedReader(new InputStreamReader(socketServer.getInputStream()));
+
+        isDeviceOkay = in.readLine();
+        int deviceOkayInt = Integer.parseInt(isDeviceOkay);
+
+        if (deviceOkayInt == 1) {
+
+          // if ok
+          this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_OK);
+        }
+        else {
+          // if not ok
+          this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_NOT_OKAY);
+        }
+
+      }
+      catch (Exception e) {
+        System.out.println(e.getMessage());
+        // if not ok
+        this.deviceManager.reportDeviceStatus(this.deviceToMonitor, DeviceManager.DEVICE_STATUS_NOT_OKAY);
+
+      }
+
+      try {
+        socketServer.close();
+      }
+      catch (IOException e) {
+        System.out.println(e.getMessage());
+      }
+
+    }
+
+  }
 }
--- src/RegistrationWorker.java
+++ src/RegistrationWorker.java
@@ -6,142 +6,104 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class RegistrationWorker extends Thread {
+public class RegistrationWorker implements Runnable {
 
-	ServerSocket mSocket;
-	int mPortNumber = 4000;
-	DeviceManager deviceManager;
+  ServerSocket mSocket;
+  int mPortNumber = 4000;
+  DeviceManager deviceManager;
 
-	public RegistrationWorker(DeviceManager deviceManger) {
-		
-		
-		this.deviceManager = deviceManger;
-		
-		boolean isPortAvailable = false;
-		
-		while (!isPortAvailable){
+  public RegistrationWorker(DeviceManager deviceManger) {
+    this.deviceManager = deviceManger;
+    boolean isPortAvailable = false;
+    while (!isPortAvailable) {
+      try {
+        mSocket = new ServerSocket(mPortNumber);
+        isPortAvailable = true;
+      }
+      catch (IOException e) {
+        mPortNumber++;
+        if (mPortNumber > 7000) {
+          System.out.println("Could not open a socket");
+          System.exit(-1);
+        }
+      }
+    }
+  }
 
-		try {
-			mSocket = new ServerSocket(mPortNumber);
-			isPortAvailable = true;
-		} catch (IOException e) {
-		
-			mPortNumber++;
-			
-			if (mPortNumber > 7000){
-				System.out.println("Could not open a socket");
-				System.exit(-1);
-			}
-		}
-		}
-	}
+  @Override
+  public void run() {
+    try {
+      InetAddress ip = null;
+      try {
+        ip = InetAddress.getLocalHost();
+      }
+      catch (UnknownHostException e1) {
+      }
+      System.out.println("Registration Worker IP address : " + ip.getHostAddress());
+      System.out.println("Registration Worker Listening on port: " + mPortNumber);
 
-	@Override
-	public void run() {
+      while (true) {
 
-		
-		
-		
-		InetAddress ip = null;
-		try {
-			ip = InetAddress.getLocalHost();
-		} catch (UnknownHostException e1) {
-		}
-		System.out.println("Registration Worker IP address : " + ip.getHostAddress());
-		System.out.println("Registration Worker Listening on port: "+ mPortNumber);
+        // accept requests
+        Socket clientSocket = null;
 
-	
-		while (true) {
-			
-			
-			//accept requests
-			Socket clientSocket = null;
-			
-			try {
-				clientSocket = mSocket.accept();
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Could not connect to socket");
-			}
-			
-			
-			
-			//read from socket
-		/*	try {
-	            BufferedReader in = 
-	            new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	            String deviceId = in.readLine();
-	            deviceId = in.readLine();
-	            deviceId = in.readLine();
-	            deviceId = in.readLine();
-	            deviceId = in.readLine();
-	            
-	            //TODO: HANDLE REGISTRATION
-	            System.out.println("Device Registration ID: " + deviceId);
-	           // in.close();
-	        } catch (IOException e) {
-	            //Client disconnected
-	            System.out.println("Client disconnected.");
-	            break;
-	        }*/
-			String line = "";
-			BufferedReader in = null;
-			try {
-				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try {
+          clientSocket = mSocket.accept();
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+          System.out.println("Could not connect to socket");
+        }
 
-				String ipAddress = in.readLine();
-				int portNumber = Integer.parseInt(in.readLine());
-				String deviceID = in.readLine();
+        // read from socket
+        /*
+         * try { BufferedReader in = new BufferedReader(new
+         * InputStreamReader(clientSocket.getInputStream())); String deviceId =
+         * in.readLine(); deviceId = in.readLine(); deviceId = in.readLine();
+         * deviceId = in.readLine(); deviceId = in.readLine();
+         * 
+         * //TODO: HANDLE REGISTRATION
+         * System.out.println("Device Registration ID: " + deviceId); //
+         * in.close(); } catch (IOException e) { //Client disconnected
+         * System.out.println("Client disconnected."); break; }
+         */
+        //String line = "";
+        BufferedReader in = null;
+        try {
+          in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-				Device device = new Device(ipAddress,portNumber,deviceID);
-				deviceManager.registerDevice(device);
-				
+          String ipAddress = in.readLine();
+          int portNumber = Integer.parseInt(in.readLine());
+          String deviceID = in.readLine();
 
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-			    System.out.println("Read failed");
-		        System.exit(-1);
-			}
-	
-			//close client
-			try {
-				clientSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+          Device device = new Device(ipAddress, portNumber, deviceID);
+          deviceManager.registerDevice(device);
 
-	}
-	
-	
-	public void cleanUp(){
-		
-		
-		
-		
-		
-		//close socket
-		try {
-			mSocket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		//stop thread
-				try {
-					this.join();
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				this.interrupt();
-	}
-	
-	
-	
-	
+        }
+        catch (IOException e1) {
+          // TODO Auto-generated catch block
+          System.out.println("Read failed");
+          System.exit(-1);
+        }
+
+        // close client
+        try {
+          clientSocket.close();
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    finally {
+      // close socket
+      try {
+        mSocket.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 }
