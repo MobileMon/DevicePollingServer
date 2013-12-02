902e1a70d24c2f275402f5e023f0dbf40289dfe5
Ryan <ryanrchesla@gmail.com>
2013-12-02 11:14:06 -0500

    merged johns with master

--- src/TimerExtension.java
+++ src/TimerExtension.java
@@ -3,12 +3,6 @@ import java.util.Timer;
 public class TimerExtension
 extends Timer {
 
-<<<<<<< HEAD
   public boolean hasConnected;
 
-=======
-public class TimerExtension extends Timer {
-	
-	public boolean hasConnected;
->>>>>>> newbranch
 }
